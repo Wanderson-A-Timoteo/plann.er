2b671ab2368d5931029066e25af61b630d86d212
Wanderson A. Timóeo <wanderson_timoteo@hotmail.com>
2024-08-05 19:55:53 -0400

    Updated TripDetailsPage component with formatting changes and new UI elements

--- planner-frontend/src/pages/create-trip/index.tsx
+++ planner-frontend/src/pages/create-trip/index.tsx
@@ -99,8 +99,8 @@ export function CreateTripPage() {
 
         <p className="text-sm text-zinc-500">
           Ao planejar sua viagem pela plann.er você automaticamente concorda <br />
-          com nossos <a className="text-zinc-300 underline" href="#"> termos de uso</a> e 
-          <a className="text-zinc-300 underline" href="#"> política de privacidade </a>.
+          com nossos <a className="text-zinc-300 underline" href="#"> termos de uso </a> e 
+          <a className="text-zinc-300 underline" href="#"> política de privacidade. </a>
         </p>
       </div>
 
