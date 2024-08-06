fa6a757b1697d226b5b23e1fba69d9613a24bbd0
Wanderson A. Timóeo <wanderson_timoteo@hotmail.com>
2024-08-05 20:26:48 -0400

    add left side of important links

--- planner-frontend/src/pages/trip-details/index.tsx
+++ planner-frontend/src/pages/trip-details/index.tsx
@@ -1,7 +1,8 @@
-import { Calendar, CircleCheck, MapPin, Plus, Settings2 } from "lucide-react"
+import { Calendar, CircleCheck, Link2, MapPin, Plus, Settings2 } from "lucide-react"
 
 export function TripDetailsPage() {
   return (
+    /* Head */
     <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
       <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
         <div className="flex items-center gap-2">
@@ -21,6 +22,7 @@ export function TripDetailsPage() {
         </div>
       </div>
       <main className="flex gap-16 px-4">
+        {/* right side */}
         <div className="flex-1 space-y-6">
           <div className="flex items-center justify-between">
             <h2 className="text-3xl font-semibold">Atividades</h2>
@@ -61,8 +63,38 @@ export function TripDetailsPage() {
             </div>
           </div>
         </div>
-        <div className="w-80">
+        
+        {/* left side */}
+        <div className="w-80 space-y-6">
+          <div className="space-y-6">
+            <h2 className="font-semibold text-xl">Links Importantes</h2>
+            <div className="space-y-5 ">
+              <div className="flex items-center justify-between gap-4">
+                <div className="space-y-1.5">
+                  <span className="block font-medium text-zinc-100">Reserva do AirBnB</span>
+                  <a className="block text-xs text-zinc-400 truncate hover:text-zinc-200 cursor-pointer">
+                    https://www.airbnb.com.br/rooms/104700011465416498+498498498494
+                  </a>
+                </div>
+                <Link2 className="size-5 text-zinc-400 shrink-0" />
+              </div>
 
+              <div className="flex items-center justify-between gap-4">
+                <div className="space-y-1.5">
+                  <span className="block font-medium text-zinc-100">Reserva do AirBnB</span>
+                  <a className="block text-xs text-zinc-400 truncate hover:text-zinc-200 cursor-pointer">
+                    https://www.airbnb.com.br/rooms/104700011465416498+498498498494
+                  </a>
+                </div>
+                <Link2 className="size-5 text-zinc-400 shrink-0" />
+              </div>
+            </div>
+            <button className="w-full bg-zinc-800 text-zinc-200 justify-center rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700">
+              <Plus className='size-5'/>
+              Cadastrar novo link
+            </button>
+          </div>
+          <div className="w-full h-px bg-zinc-800" />
         </div>
       </main>
     </div>
